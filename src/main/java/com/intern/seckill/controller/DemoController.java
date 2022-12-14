6c5a9d551103c7f082ef64c2ee44f317b6d18ec1
RicardoAGu <2374867967@qq.com>
2022-12-15 06:34:02 +0800

    搞了一夜，可算做出来一个页面了。。。刚才的提交文字编码有点问题，再提交一遍

--- src/main/java/com/intern/seckill/controller/DemoController.java
+++ src/main/java/com/intern/seckill/controller/DemoController.java
@@ -1,2 +1,30 @@
-package com.intern.seckill.controller;public class DemoController {
+package com.intern.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 测试
+ * @author Ricardo.A.Gu
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping("/demo")
+/*
+RequestMapping是一个用来处理请求地址映射的注解，可用于类或方法上。
+用于类上，表示类中的所有响应请求的方法都是以该地址作为父路径。
+(RequestMapping注解的作用是建立请求URL和处理方法之间的对应关系)
+ */
+public class DemoController {
+    /**
+     * 功能描述：测试页面跳转
+     * @author Ricardo.A.Gu
+     * @since 1.0.0
+     */
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "顾傲广");
+        return "hello";
+    }
 }
